2b0b3414b3220579c971fa47596e10905cf0ab14
duyanpu <duyanpu_dz@mayi.com>
2018-01-18 16:08:23 +0800

    fix

--- Order_predicts/preprocessing.py
+++ Order_predicts/preprocessing.py
@@ -8,6 +8,7 @@ from pyduyp.utils.utils import time2day, time2mouth, time2week
 from pyduyp.utils.utils import compute_interval_of_day
 from pyduyp.utils.utils import get_freq_of_day_and_month, get_week_freq, get_type_freq
 from pyduyp.logger.log import log
+
 log.info("Start runing ...")
 
 
@@ -112,7 +113,7 @@ def get_action_features(step='train'):
         actions = []
         for file in tqdm(os.listdir(root)):
             data = pd.read_csv(os.path.join(root, file))
-            data_types = data['actionType'].values
+            data_types = data['actionType'].values.tolist()
             data_copy = data.copy()
             data_copy['time2days'] = data['actionTime'].apply(time2day)
             data_copy['time2mouth'] = data['actionTime'].apply(time2mouth)
@@ -131,7 +132,7 @@ def get_action_features(step='train'):
                 rows['0_label'] = 0
             aid = file.split(".")[0]
             rows['1_id'] = aid
-            rows['2_t1'] = type_freq['2_t1'] # 类型1－9点击总数
+            rows['2_t1'] = type_freq['2_t1']  # 类型1－9点击总数
             rows['3_t2'] = type_freq['3_t2']
             rows['4_t3'] = type_freq['4_t3']
             rows['5_t4'] = type_freq['5_t4']
@@ -140,17 +141,17 @@ def get_action_features(step='train'):
             rows['8_t7'] = type_freq['8_t7']
             rows['9_t8'] = type_freq['9_t8']
             rows['10_t9'] = type_freq['10_t9']
-            rows['11_rate1'] = type_freq['2_t1']/types_sum  # 打开app的比例
-            rows['12_rate9'] = type_freq['10_t9']/types_sum  # 下单的比例
+            rows['11_rate1'] = type_freq['2_t1'] / types_sum  # 打开app的比例
+            rows['12_rate9'] = type_freq['10_t9'] / types_sum  # 下单的比例
             rows['13_atmean'] = np.mean(time_counts)  # 时间均值
             rows['14_atstd'] = np.std((time_counts))  # 时间标准差
             rows['15_atmedian'] = np.median(time_counts)  # 时间中位数
             rows['16_tmode'] = mode(time_counts)  # 时间众数
-            rows['17_atptp'] = np.ptp(time_counts)  # 时间极差
+            rows['17_atptp'] = np.max(time_counts) - np.min(time_counts) if len(time_counts) > 0 else 0  # 时间极差
             rows['18_atvar'] = np.var(time_counts)  # 时间方差
-            rows['19_xishu'] = np.mean(time_counts)/np.std(time_counts)  # 时间变异系数
-            rows['20_lastmean'] = np.mean(time_counts[-1:-4])  #　最后三天间隔的均值
-            rows['21_laststd'] = np.std(time_counts[-1:-4]) #　最后三天间隔的标准差
+            rows['19_xishu'] = np.mean(time_counts) / np.std(time_counts) if len(time_counts) > 0 else 0  # 时间变异系数
+            rows['20_lastmean'] = np.mean(time_counts[-1:-4])  # 最后三天间隔的均值
+            rows['21_laststd'] = np.std(time_counts[-1:-4])  # 最后三天间隔的标准差
             rows['22_dayrate'] = get_freq_of_day_and_month(data_copy_grouped_day)  # 日均
             rows['23_monthrate'] = get_freq_of_day_and_month(data_copy_grouped_month)  # 月均
             rows['24_weekrate'] = get_week_freq(data_copy['time2week'].values)  # 周均
@@ -158,16 +159,16 @@ def get_action_features(step='train'):
             rows['26_atstd'] = np.std((data_types))  # 类型标准差
             rows['27_atmedian'] = np.median(data_types)  # 类型中位数
             rows['28_tmode'] = mode(data_types)  # 类型众数
-            rows['29_atptp'] = np.ptp(data_types)  # 类型极差
+            rows['29_atptp'] = np.max(data_types) - np.min(data_types) if len(data_types) > 0 else 0  # 类型极差
             rows['30_atvar'] = np.var(data_types)  # 类型方差
-            rows['31_xishu'] = np.mean(data_types)/np.std(data_types)  # 类型变异系数
-            rows['32_rate2'] = type_freq['3_t2']/types_sum  # 2的比例
-            rows['33_rate3'] = type_freq['4_t3']/types_sum  # 3的比例
-            rows['34_rate4'] = type_freq['5_t4']/types_sum  # 4的比例
-            rows['35_rate5'] = type_freq['6_t5']/types_sum  # 5的比例
-            rows['36_rate6'] = type_freq['7_t6']/types_sum  # 6的比例
-            rows['37_rate7'] = type_freq['8_t7']/types_sum  # 7的比例
-            rows['38_rate8'] = type_freq['9_t8']/types_sum  # 8的比例
+            rows['31_xishu'] = np.mean(data_types) / np.std(data_types) if len(data_types) > 0 else 0  # 类型变异系数
+            rows['32_rate2'] = type_freq['3_t2'] / types_sum  # 2的比例
+            rows['33_rate3'] = type_freq['4_t3'] / types_sum  # 3的比例
+            rows['34_rate4'] = type_freq['5_t4'] / types_sum  # 4的比例
+            rows['35_rate5'] = type_freq['6_t5'] / types_sum  # 5的比例
+            rows['36_rate6'] = type_freq['7_t6'] / types_sum  # 6的比例
+            rows['37_rate7'] = type_freq['8_t7'] / types_sum  # 7的比例
+            rows['38_rate8'] = type_freq['9_t8'] / types_sum  # 8的比例
 
             actions.append(rows)
 
