275828eae4fbae88ffe0f401d384dc6497d7c3cc
duyanpu <duyanpu_dz@mayi.com>
2018-01-19 17:57:06 +0800

    fix

--- Order_predicts/main.py
+++ Order_predicts/main.py
@@ -43,7 +43,7 @@ def minibatches(inputs=None, targets=None, batch_size=None, shuffle=False):
 
 def randomforest():
     # 　特征选择
-    pos = pd.read_csv("Order_predicts/datasets/results/train/action_pos_features.csv", usecols=['14_atstd'])
+    pos = pd.read_csv("Order_predicts/datasets/results/train/action_pos_features.csv")
     posfillna = pos.fillna(pos.median()).replace(np.inf, 100)
     neg = pd.read_csv("Order_predicts/datasets/results/train/action_neg_features.csv")
     negfillna = neg.fillna(neg.median()).replace(np.inf, 100)
@@ -67,24 +67,15 @@ def randomforest():
 
 def train_models(model_name, epoch=5, batch_size=100):
     pos = pd.read_csv("Order_predicts/datasets/results/train/action_pos_features.csv")
+    posfillna = pos.fillna(pos.median()).replace(np.inf, 100)
     neg = pd.read_csv("Order_predicts/datasets/results/train/action_neg_features.csv")
-    data = pd.concat([pos, neg])
+    negfillna = neg.fillna(neg.median()).replace(np.inf, 100)
+    data = pd.concat([posfillna, negfillna])
     data = shuffle(data)
-    del data['16_tmode']  # 删除不重要的特征　！
-    del data['10_t9']
-    del data['28_tmode']
-    del data['27_atmedian']
-    del data['29_atptp']
-    del data['continent']
-    del data['province']
-    del data['country']
-    del data['city']
-    del data['age']
     del data['id']
-
     y = data['label']
     del data['label']
-    X = data.fillna(data.mean(), axis=1).replace(np.inf, 100)
+    X = data
     scaler = preprocessing.StandardScaler().fit(X)
     scaler.transform(X)
     X.to_csv("Order_predicts/datasets/results/scale_x.csv", index=None)
@@ -102,21 +93,21 @@ def train_models(model_name, epoch=5, batch_size=100):
                                       tol=1e-3, cache_size=200, class_weight={1: 10},
                                       verbose=False, max_iter=-1, decision_function_shape='ovr',
                                       random_state=0)
-            if model_name == 'svr':
+            elif model_name == 'svr':
                 clf_weights = svm.SVR(kernel='rbf', degree=3, gamma='auto', coef0=0.0,
                                       tol=1e-3, C=1.0, epsilon=0.1, shrinking=True,
                                       cache_size=200, verbose=False, max_iter=-1)
-            if model_name == 'lasso':
+            elif model_name == 'lasso':
                 clf_weights = Lasso(alpha=1.0, fit_intercept=True, normalize=False,
                                     precompute=False, copy_X=True, max_iter=1000,
                                     tol=1e-4, warm_start=False, positive=False,
                                     random_state=0, selection='cyclic')
-            if model_name == 'logistic':
+            elif model_name == 'logistic':
                 clf_weights = LogisticRegression(penalty='l2', dual=False, tol=1e-4, C=1.0,
                                                  fit_intercept=True, intercept_scaling=1, class_weight={1: 10},
                                                  random_state=0, solver='liblinear', max_iter=100,
                                                  multi_class='ovr', verbose=0, warm_start=False, n_jobs=1)
-            if model_name == 'mlpr':
+            elif model_name == 'mlpr':
                 # learning_rate: {'constant', 'invscaling', 'adaptive'}
                 clf_weights = MLPRegressor(hidden_layer_sizes=(100,), activation="logistic",
                                            solver='adam', alpha=0.0001,
@@ -128,7 +119,7 @@ def train_models(model_name, epoch=5, batch_size=100):
                                            nesterovs_momentum=True, early_stopping=False,
                                            validation_fraction=0.1, beta_1=0.9, beta_2=0.999,
                                            epsilon=1e-8)
-            if model_name == 'rf':
+            elif model_name == 'rf':
                 clf_weights = RandomForestClassifier(n_estimators=10, criterion="gini",
                                                      max_depth=None, min_samples_split=2,
                                                      min_samples_leaf=1, min_weight_fraction_leaf=0.,
@@ -136,11 +127,11 @@ def train_models(model_name, epoch=5, batch_size=100):
                                                      min_impurity_decrease=0., min_impurity_split=None,
                                                      bootstrap=True, oob_score=False, n_jobs=1, random_state=0,
                                                      verbose=0, warm_start=False, class_weight=None)
-            if model_name == 'adaboost':
+            elif model_name == 'adaboost':
                 clf_weights = AdaBoostClassifier(base_estimator=None, n_estimators=50, learning_rate=1.,
                                                  algorithm='SAMME.R', random_state=0)
 
-            if model_name == 'gbr':
+            elif model_name == 'gbr':
                 clf_weights = GradientBoostingRegressor(loss='ls', learning_rate=0.1, n_estimators=100,
                                                         subsample=1.0, criterion='friedman_mse', min_samples_split=2,
                                                         min_samples_leaf=1, min_weight_fraction_leaf=0.,
@@ -148,35 +139,35 @@ def train_models(model_name, epoch=5, batch_size=100):
                                                         min_impurity_split=None, init=None, random_state=0,
                                                         max_features=None, alpha=0.9, verbose=0, max_leaf_nodes=None,
                                                         warm_start=False, presort='auto')
-            if model_name == 'qda':
+            elif model_name == 'qda':
                 clf_weights = QuadraticDiscriminantAnalysis(priors=None, reg_param=0., store_covariance=False,
                                                             tol=1.0e-4, store_covariances=None)
-            if model_name == 'lda':
+            elif model_name == 'lda':
                 clf_weights = LinearDiscriminantAnalysis(solver='svd', shrinkage=None, priors=None,
                                                          n_components=None, store_covariance=False, tol=1e-4)
-            if model_name == 'n_n':
+            elif model_name == 'n_n':
                 clf_weights = NearestNeighbors(n_neighbors=5, radius=1.0,
                                                algorithm='auto', leaf_size=30, metric='minkowski',
                                                p=2, metric_params=None, n_jobs=1)
-            if model_name == 'gnb':
+            elif model_name == 'gnb':
                 clf_weights = GaussianNB(priors=None)
 
-            if model_name == 'bnb':
+            elif model_name == 'bnb':
                 clf_weights = BernoulliNB(alpha=1.0, binarize=.0, fit_prior=True, class_prior=None)
-            if model_name == 'dcc':
+            elif model_name == 'dcc':
                 clf_weights = DecisionTreeClassifier(criterion="gini", splitter="best", max_depth=None,
                                                      min_samples_split=2, min_samples_leaf=1,
                                                      min_weight_fraction_leaf=0., max_features=None,
                                                      random_state=0, max_leaf_nodes=None,
                                                      min_impurity_decrease=0., min_impurity_split=None,
                                                      class_weight=None, presort=False)
-            if model_name == 'dcr':
+            elif model_name == 'dcr':
                 clf_weights = DecisionTreeRegressor(criterion="mse", splitter="best", max_depth=None,
                                                     min_samples_split=2, min_samples_leaf=1,
                                                     min_weight_fraction_leaf=0., max_features=None,
                                                     random_state=0, max_leaf_nodes=None,
                                                     min_impurity_decrease=0., min_impurity_split=None, presort=False)
-            if model_name == 'RAN':
+            elif model_name == 'RAN':
                 base_estimator = LinearRegression()
                 clf_weights = RANSACRegressor(base_estimator=base_estimator, min_samples=None,
                                               residual_threshold=None, is_data_valid=None,
@@ -184,7 +175,7 @@ def train_models(model_name, epoch=5, batch_size=100):
                                               stop_n_inliers=np.inf, stop_score=np.inf,
                                               stop_probability=0.99, residual_metric=None,
                                               loss='absolute_loss', random_state=0)
-            if model_name == 'SGDR':
+            else:  # model_name == 'SGDR':
                 clf_weights = SGDRegressor(loss="squared_loss", penalty="l2", alpha=0.0001,
                                            l1_ratio=0.15, fit_intercept=True, max_iter=None, tol=None,
                                            shuffle=True, verbose=0, epsilon=0.1,
@@ -213,29 +204,19 @@ def train_models(model_name, epoch=5, batch_size=100):
 
 def modeltest(model_name='svm'):
     pos = pd.read_csv("Order_predicts/datasets/results/test/action_pos_features.csv")
+    posfillna = pos.fillna(pos.median()).replace(np.inf, 100)
+
     neg = pd.read_csv("Order_predicts/datasets/results/test/action_neg_features.csv")
-    data = pd.concat([pos, neg])
+    negfillna = neg.fillna(neg.median()).replace(np.inf, 100)
+    data = pd.concat([posfillna, negfillna])
     data = shuffle(data)
     ids = data['id'].values.tolist()
-    data = data.fillna(-1).replace(np.inf, 100)
-    del data['16_tmode']
-    del data['10_t9']
-    del data['28_tmode']
-    del data['27_atmedian']
-    del data['29_atptp']
-    del data['continent']
-    del data['province']
-    del data['country']
-    del data['city']
-    del data['age']
-
-    data = data.fillna(-1).replace(np.inf, 100)
     df_push = pd.DataFrame()
     linenumber = 0
     for i in tqdm(ids):
         batch_x = data[data['id'].isin([i])]
         del batch_x['id']
-        clf_weights = joblib.load("Order_predicts/datasets/results/models/{}_1_30.model".format(model_name))
+        clf_weights = joblib.load("Order_predicts/datasets/results/models/{}/1_30.model".format(model_name))
 
         # p = clf_weights.predict(batch_x.values)
         prob = clf_weights.predict_proba(batch_x.values)[0]
@@ -261,7 +242,6 @@ if __name__ == "__main__":
                    'lda', 'n_n', 'gnb', 'bnb', 'dcc', 'RAN', 'SGDR']
         log.info("Total number models: {}".format(len(m_names)))
 
-        for name in m_names:
-            train_models(model_name='{}'.format(name), epoch=2, batch_size=1000)
+        train_models(model_name='mlpr', epoch=5, batch_size=2000)
     if method == "test":
-        modeltest(model_name='adaboost')
+        modeltest(model_name='mlpr')
