135dee0e841c985093c41a93e8fa7007a3d19f23
duyanpu <duyanpu_dz@mayi.com>
2018-01-25 11:07:16 +0800

    fix

---
+++ python_learn/word2cloud.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+import wordcloud
+
+
+def plot_word_cloud():
+    text = open("test_file.txt", 'r').read()
+
+    alice_coloring = imread("back.png")
+
+    wc = wordcloud.WordCloud(background_color="white",
+                             mask=alice_coloring,
+                             max_font_size=20,
+                             random_state=1,
+                             max_words=100,
+                             font_path='/home/duyp/fonts/msyh.ttf')
+    wc.generate(text)
+    image_colors = wordcloud.ImageColorGenerator(alice_coloring)
+
+    plt.axis("off")
+    plt.figure()
+    plt.imshow(wc.recolor(color_func=image_colors))
+    plt.axis("off")
+    plt.figure()
+    plt.axis("off")
+    wc.to_file("wordcloud.png")
+
+
+plot_word_cloud()
