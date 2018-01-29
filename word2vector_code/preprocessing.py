404340764a3c1fb787b2078482c7036434ec0ddf
duyanpu <duyanpu_dz@mayi.com>
2018-01-29 20:30:14 +0800

    update

---
+++ word2vector_code/preprocessing.py
@@ -0,0 +1,82 @@
+import random
+
+import jieba
+import collections
+
+
+def skipgrams(sequence, vocabulary_size, window_size=4, negative_samples=1., shuffle=True,
+              categorical=False, sampling_table=None, seed=None):
+    couples = []
+    labels = []
+    for i, wi in enumerate(sequence):
+        if not wi:
+            continue
+        if sampling_table is not None:
+            if sampling_table[wi] < random.random():
+                continue
+
+        window_start = max(0, i - window_size)
+        window_end = min(len(sequence), i + window_size + 1)
+        for j in range(window_start, window_end):
+            if j != i:
+                wj = sequence[j]
+                if not wj:
+                    continue
+                couples.append([wi, wj])
+                if categorical:
+                    labels.append([0, 1])
+                else:
+                    labels.append(1)
+
+    if negative_samples > 0:
+        num_negative_samples = int(len(labels) * negative_samples)
+        words = [c[0] for c in couples]
+        random.shuffle(words)
+
+        couples += [[words[i % len(words)],
+                     random.randint(1, vocabulary_size - 1)] for i in range(num_negative_samples)]
+        if categorical:
+            labels += [[1, 0]] * num_negative_samples
+        else:
+            labels += [0] * num_negative_samples
+
+    if shuffle:
+        if seed is None:
+            seed = random.randint(0, 10e6)
+        random.seed(seed)
+        random.shuffle(couples)
+        random.seed(seed)
+        random.shuffle(labels)
+
+    return couples, labels
+
+
+def build_dataset(words, n_words):
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(n_words - 1))
+    dictionary = {}
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = []
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reversed_dictionary
+
+
+if __name__ == "__main__":
+    seq = '那个布拉格的房间是不是有小阳台'
+    seq_cut = jieba.lcut(seq)
+    print(seq_cut)
+    seq = " ".join(seq_cut)
+    couples, labels = skipgrams(seq, vocabulary_size=2)
+    data, count, dictionary, reversed_dictionary = build_dataset(seq_cut, 6)
+    print(dictionary)
+
