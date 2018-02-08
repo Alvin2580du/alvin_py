3615a369924bbcfc530e61bc7e7ac31c4cb4298e
duyanpu <duyanpu_dz@mayi.com>
2018-02-08 10:30:45 +0800

    updates

--- word2vector_code/preprocessing.py
+++ word2vector_code/preprocessing.py
@@ -10,7 +10,6 @@ import pandas as pd
 from tqdm import tqdm
 from gensim.models import word2vec
 from gensim.models.keyedvectors import KeyedVectors
-
 import sys
 import codecs
 import numpy as np
@@ -19,14 +18,10 @@ from sklearn.manifold import TSNE
 
 
 from pyduyp.logger.log import log
+from pyduyp.preprocessing.Zhsegment import cut
+from pyduyp.utils.utils import replace_symbol
 
 root_path = os.path.dirname(os.path.realpath(__file__))
-
-jieba.load_userdict(os.path.join(root_path, "datasets/jieba_dict_sorted.csv"))
-
-jieba.analyse.set_stop_words(os.path.join(root_path, 'datasets/stopwords_zh.csv'))
-sw = pd.read_csv(os.path.join(root_path, 'datasets/stopwords_zh.csv'), lineterminator="\n").values.tolist()
-sw2list = [j for i in sw for j in i]
 
 
 def skipgrams(sequence, vocabulary_size, window_size=4, negative_samples=1., shuffle=True,
@@ -144,21 +139,21 @@ class TextBatch(object):
         self.max_sentence_length = max_sentence_length
 
     def __iter__(self):
-        # the entire corpus is one gigantic line -- there are no sentence marks at all
-        # so just split the sequence of tokens arbitrarily: 1 sentence = 1000 tokens
         sentence, rest = [], b''
         with smart_open(self.fname) as fin:
             while True:
-                text = rest + fin.readline()  # avoid loading the entire file (=1 line) into RAM
+                text = rest + fin.readline()
                 if text == rest:  # EOF
                     words = to_unicode(text).split()
-                    sentence.extend(words)  # return the last chunk of words, too (may be shorter/longer)
+                    for word in words:
+                        if str(word).isdigit():
+                            continue
+                        sentence.extend(word)
                     if sentence:
                         yield sentence
                     break
-                last_token = text.rfind(b' ')  # last token may have been split in two... keep for next iteration
-                words, rest = (
-                    to_unicode(text[:last_token]).split(), text[last_token:].strip()) if last_token >= 0 else ([], text)
+                last_token = text.rfind(b' ')
+                words, rest = (to_unicode(text[:last_token]).split(), text[last_token:].strip()) if last_token >= 0 else ([], text)
                 sentence.extend(words)
                 while len(sentence) >= self.max_sentence_length:
                     yield sentence[:self.max_sentence_length]
@@ -207,10 +202,9 @@ def cut_data():
     with open(data_name, 'r') as fr:
         lines = fr.readlines()
         for line in tqdm(lines):
-            line_cut = jieba.lcut(line)
+            line_cut = cut(replace_symbol(line), add_stopwords=True)
             for x in line_cut:
-                if x not in sw2list:
-                    out.append(x)
+                out.append(x)
     log.info(" Length: {} ".format(len(out)))
     fw = open(os.path.join(root_path, "datasets/cd.txt"), 'w')
     fw.writelines(" ".join(out))
@@ -238,6 +232,29 @@ def load_embeddings(file_name):
     return wv, vocabulary
 
 
+def read_newdata():
+    path = '/home/duyp/mayi_datasets/question/question_new'
+    number = 0
+
+    for file in os.listdir(path):
+        filename = os.path.join(path, file)
+        data = pd.read_csv(filename, lineterminator="\n").values
+        out = []
+        for x in tqdm(data):
+            msg = x[0]
+            if isinstance(msg, str):
+                msgcut = cut(replace_symbol(msg), add_stopwords=True)
+                for i in msgcut:
+                    out.append(i)
+                    number += 1
+            else:
+                continue
+        fw = open(os.path.join(root_path, "datasets/rawdata/{}".format(file)), 'w')
+        fw.writelines(" ".join(out))
+        fw.close()
+    log.info("{}".format(number))
+
+
 if __name__ == "__main__":
     import sys
 
@@ -250,40 +267,45 @@ if __name__ == "__main__":
         log.info(" ! Build Success ! ")
 
     if method == 'train':
-        MAX_WORDS_IN_BATCH = 10000
+        MAX_WORDS_IN_BATCH = 20000
+        path = 'word2vector_code/datasets/rawdata'
+        for file in os.listdir(path):
+            data_name = os.path.join(path, file)
+            sentences = TextBatch(fname=data_name, max_sentence_length=1000)
+            model = word2vec.Word2Vec(sentences, size=128, alpha=0.025, window=3, min_count=2,
+                                      max_vocab_size=1024, sample=1e-3, seed=1, workers=4, min_alpha=0.0001,
+                                      sg=1, hs=0, negative=5, cbow_mean=1, hashfxn=hash, iter=10, null_word=0,
+                                      trim_rule=None, sorted_vocab=1, batch_words=MAX_WORDS_IN_BATCH, compute_loss=False)
+            """
+            ·  sentences：可以是一个·ist，对于大语料集，建议使用BrownCorpus,Text8Corpus或·ineSentence构建。
+            ·  sg： 用于设置训练算法，默认为0，对应CBOW算法；sg=1则采用skip-gram算法。
+            ·  size：是指特征向量的维度，默认为100。大的size需要更多的训练数据,但是效果会更好. 推荐值为几十到几百。
+            ·  window：表示当前词与预测词在一个句子中的最大距离是多少
+            ·  alpha: 是学习速率
+            ·  seed：用于随机数发生器。与初始化词向量有关。
+            ·  min_count: 可以对字典做截断. 词频少于min_count次数的单词会被丢弃掉, 默认值为5
+            ·  max_vocab_size: 设置词向量构建期间的RAM限制。如果所有独立单词个数超过这个，则就消除掉其中最不频繁的一个。
+                               每一千万个单词需要大约1GB的RAM。设置成None则没有限制。
+            ·  sample: 高频词汇的随机降采样的配置阈值，默认为1e-3，范围是(0,1e-5)
+            ·  workers: 参数控制训练的并行数。
+            ·  hs: 如果为1则会采用hierarchica·softmax技巧。如果设置为0（defau·t），则negative sampling会被使用。
+            ·  negative: 如果>0,则会采用negativesamp·ing，用于设置多少个noise words
+            ·  cbow_mean: 如果为0，则采用上下文词向量的和，如果为1（defau·t）则采用均值。只有使用CBOW的时候才起作用。
+            ·  hashfxn： hash函数来初始化权重。默认使用python的hash函数
+            ·  iter： 迭代次数，默认为5
+            ·  trim_rule： 用于设置词汇表的整理规则，指定那些单词要留下，哪些要被删除。
+                            可以设置为None（min_count会被使用）或者一个接受()并返回RU·E_DISCARD,uti·s.RU·E_KEEP或者uti·s.RU·E_DEFAU·T的函数。
+            ·  sorted_vocab： 如果为1（defau·t），则在分配word index 的时候会先对单词基于频率降序排序。
+            ·  batch_words：每一批的传递给线程的单词的数量，默认为10000
+            """
+            save_dir = 'datasets/results/vector'
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
 
-        data_name = os.path.join(root_path, "datasets/cd.txt")
-        sentences = TextBatch(fname=data_name, max_sentence_length=1000)
-        model = word2vec.Word2Vec(sentences, size=128, alpha=0.025, window=5, min_count=5,
-                                  max_vocab_size=1024, sample=1e-3, seed=1, workers=3, min_alpha=0.0001,
-                                  sg=0, hs=0, negative=5, cbow_mean=1, hashfxn=hash, iter=2, null_word=0,
-                                  trim_rule=None, sorted_vocab=1, batch_words=MAX_WORDS_IN_BATCH, compute_loss=False)
-        """
-        ·  sentences：可以是一个·ist，对于大语料集，建议使用BrownCorpus,Text8Corpus或·ineSentence构建。
-        ·  sg： 用于设置训练算法，默认为0，对应CBOW算法；sg=1则采用skip-gram算法。
-        ·  size：是指特征向量的维度，默认为100。大的size需要更多的训练数据,但是效果会更好. 推荐值为几十到几百。
-        ·  window：表示当前词与预测词在一个句子中的最大距离是多少
-        ·  alpha: 是学习速率
-        ·  seed：用于随机数发生器。与初始化词向量有关。
-        ·  min_count: 可以对字典做截断. 词频少于min_count次数的单词会被丢弃掉, 默认值为5
-        ·  max_vocab_size: 设置词向量构建期间的RAM限制。如果所有独立单词个数超过这个，则就消除掉其中最不频繁的一个。
-                           每一千万个单词需要大约1GB的RAM。设置成None则没有限制。
-        ·  sample: 高频词汇的随机降采样的配置阈值，默认为1e-3，范围是(0,1e-5)
-        ·  workers: 参数控制训练的并行数。
-        ·  hs: 如果为1则会采用hierarchica·softmax技巧。如果设置为0（defau·t），则negative sampling会被使用。
-        ·  negative: 如果>0,则会采用negativesamp·ing，用于设置多少个noise words
-        ·  cbow_mean: 如果为0，则采用上下文词向量的和，如果为1（defau·t）则采用均值。只有使用CBOW的时候才起作用。
-        ·  hashfxn： hash函数来初始化权重。默认使用python的hash函数
-        ·  iter： 迭代次数，默认为5
-        ·  trim_rule： 用于设置词汇表的整理规则，指定那些单词要留下，哪些要被删除。
-                        可以设置为None（min_count会被使用）或者一个接受()并返回RU·E_DISCARD,uti·s.RU·E_KEEP或者uti·s.RU·E_DEFAU·T的函数。
-        ·  sorted_vocab： 如果为1（defau·t），则在分配word index 的时候会先对单词基于频率降序排序。
-        ·  batch_words：每一批的传递给线程的单词的数量，默认为10000
-        """
-        total_path = os.path.join(root_path, "datasets/cd_vectors.txt")
-        model.wv.save_word2vec_format(total_path, binary=False)
-        model.save(root_path + "/datasets/cd.model")
-        log.info(" ! Build Success ! ")
+            total_path = os.path.join(root_path, "datasets/results/vector/cd_vectors.txt")
+            model.wv.save_word2vec_format(total_path, binary=False)
+            model.save(root_path + "/datasets/cd.model")
+            log.info(" ! Build Success ! ")
 
     if method == 'test':
         # distance = model.wmdistance(sentence_obama, sentence_president)
@@ -296,3 +318,6 @@ if __name__ == "__main__":
 
     if method == 'plot':
         plot_embedding()
+
+    if method == 'newdata':
+        read_newdata()
