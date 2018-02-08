b73477f63ad778d8861ebdf7ccc1dcaf8a3e6805
duyanpu <duyanpu_dz@mayi.com>
2018-02-08 09:43:17 +0800

    update

--- pyduyp/decorators/decorator.py
+++ pyduyp/decorators/decorator.py
@@ -1,5 +1,8 @@
+from functools import wraps
 
 from functools import wraps
+from pyduyp.datasources.basic_db import db_session
+from pyduyp.logger.log import log
 
 
 def es_get_decorator(func):
@@ -9,4 +12,18 @@ def es_get_decorator(func):
             return func(*args, **kwargs)
         except Exception as e:
             print('es get response error, detail {}'.format(e))
+
     return es_get
+
+
+def db_commit_decorator(func):
+    @wraps(func)
+    def session_commit(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            log.error('db operation error, detail {}'.format(e))
+            log.warning('transaction rollbacks')
+            db_session.rollback()
+
+    return session_commit
