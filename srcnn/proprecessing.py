9a510e7c2dc4cfe400a6a3a2333831f73b870a6d
Alvin2580du <ypducdtu@163.com>
2018-03-10 16:44:45 +0800

    测试数据和训练数据准备

--- srcnn/proprecessing.py
+++ srcnn/proprecessing.py
@@ -2,6 +2,9 @@ import scipy.misc
 import scipy
 import os
 from PIL import Image
+import cv2
+
+root = "D:\\alvin_py\\srcnn\\"
 
 
 def splitimage(src, dst, target_size=256):
@@ -36,7 +39,55 @@ def build_split_data(step='train'):
         splitimage(os.path.join(path, file), save_path, target_size=256)
 
 
+def subsample_yaogan(size=64, Hr_subsample='yaogan_64x64', origin_Hr='yaogan', step='Test'):
+    save_dir = os.path.join(root, step, Hr_subsample)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    hr_dir = os.path.join(root, step, origin_Hr)
+    files = os.listdir(hr_dir)
+    for file in files:
+        fileName = os.path.join(hr_dir, file)
+        image = cv2.imread(fileName)
+        imageresize = cv2.resize(image, (size, size), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(os.path.join(save_dir, file), imageresize)
+
+
+def upsample_yaogan(step="Test", lr_upsample_path='yaogan_lr_256x256', Hr_subsample='yaogan_64x64'):
+    lr_path = os.path.join(root, step, lr_upsample_path)
+    if not os.path.exists(lr_path):
+        os.makedirs(lr_path)
+    files = os.listdir(os.path.join(root, step, Hr_subsample))
+    for file in files:
+        fileName = os.path.join(root, step, Hr_subsample, file)
+        image = cv2.imread(fileName)
+        imageresize = cv2.resize(image, (256, 256), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(os.path.join(lr_path, file), imageresize)
+
+
+def write2file_for_train(step='Test', hr_path='yaogan', lr_path='yaogan_lr_256x256'):
+    fw = open(os.path.join(root, '{}\\yaogan.txt'.format(step)), 'w', encoding='utf-8')
+    files_hr = os.listdir(os.path.join(root, step, hr_path))
+    files_lr = os.listdir(os.path.join(root, step, lr_path))
+    length = len(files_hr)
+
+    for i in range(length):
+        files_hr_name = files_hr[i]
+        files_lr_name = files_lr[i]
+        save_str = "{}|{}".format(os.path.join(root, step, hr_path, files_hr_name), os.path.join(root, step, lr_path, files_lr_name))
+        fw.writelines(save_str + "\n")
+
+
 if __name__ == "__main__":
-    method = 'first'
+    method = 'four'
+
     if method == 'first':
         build_split_data(step='test')
+
+    if method == 'second':
+        subsample_yaogan(size=64, Hr_subsample='yaogan_64x64', origin_Hr='yaogan', step='Test')
+
+    if method == 'third':
+        upsample_yaogan(step="Test", lr_upsample_path='yaogan_lr_256x256', Hr_subsample='yaogan_64x64')
+
+    if method == 'four':
+        write2file_for_train(step='Test', hr_path='yaogan', lr_path='yaogan_lr_256x256')
