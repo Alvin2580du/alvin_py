e44a7840a1a869de9589fe099e9f0a9c3c2e895e
Alvin2580du <ypducdtu@163.com>
2018-03-30 01:35:13 +0800

    fix

--- for_Order/tockets.py
+++ for_Order/tockets.py
@@ -17,10 +17,11 @@
 import pandas as pd
 from datetime import datetime, timedelta
 import os
-from tqdm import tqdm
+from tqdm import tqdm, trange
 from sklearn.linear_model import LinearRegression
 import numpy as np
 from glob import glob
+
 """
 1. 删除无用列，得到(478, 2685)维大的矩阵,文件名为train.csv。
 2. 把每个公司90天内的相关公司的数据，分组到不同的文件中，然后对每个文件计算平均值
@@ -37,16 +38,16 @@ def getday(y=2017, m=8, d=15, n=0):
     return d
 
 
-def convert(inputs):
-    return datetime.strptime(inputs.replace("\n", ""), '%Y/%m/%d').strftime("%Y-%m-%d")
-
-
 def sub356(inputs):
-    return (inputs - timedelta(days=365)).strftime('%Y-%m-%d')
+    return (inputs - timedelta(days=365)).strftime('%Y-%m-%d %H:%M:%S')
 
 
 def sub90(inputs):
-    return (inputs - timedelta(days=90)).strftime('%Y-%m-%d')
+    return (inputs - timedelta(days=90)).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def convert(inputs):
+    return datetime.strptime(inputs.replace("\n", ""), '%Y/%m/%d').strftime("%Y-%m-%d %H:%M:%S")
 
 
 def groupbycompany():
@@ -68,22 +69,44 @@ def groupbycompany():
 
 
 def rec_compute():
-    recs = []
+    recs = {}
     k = 0
-    for i in range(478):
+    for i in trange(478):
         file = os.path.join("./company", "{}.csv".format(i))
         data = pd.read_csv(file)
+        name = data['股票简称']
         del data['date']
         del data['行业代码']
         del data['日期']
         del data['股票简称']
         meanrec = data.mean().values
-        recs.append(meanrec)
+        recs[name] = meanrec
         k += 1
     df = pd.DataFrame(recs)
     df = df.fillna(0)
     df.to_csv("./data_dir/rec.csv", index=None, encoding='utf-8')
     print(k, df.shape)
+
+
+def rec_new():
+    recs = {}
+    data = pd.read_csv("./data_dir/train.csv", encoding='utf-8')
+    for index, row in data.iterrows():
+        name = row['股票简称']
+        i = str2timedate(row['日期'])
+        t1sub90 = sub90(i)
+        datelist = timestamp2list(pd.date_range(start=t1sub90, end=i, freq='D'))
+        company = data[data['日期'].apply(convert).isin(datelist)]
+        del company['股票简称']
+        del company['日期']
+        del company['行业代码']
+        means = company.mean().values
+        recs[name] = means
+    df = pd.DataFrame(recs)
+    if not os.path.exists('./data_dir'):
+        os.makedirs("./data_dir")
+    df.to_csv("./data_dir/rec.csv", index=None, encoding='utf-8')
+    print(df.shape)
 
 
 def groupbyhangye():
@@ -95,25 +118,47 @@ def groupbyhangye():
         j.to_csv("./hangye/{}.csv".format(i), index=None, encoding='utf-8')
 
 
+def timestamp2list(timestamps):
+    datelist = timestamps.to_pydatetime()
+    datelists = []
+    for d in datelist:
+        d = d.strftime("%Y-%m-%d %H:%M:%S")
+        datelists.append(d)
+    return datelists
+
+
+def str2timedate(inputs):
+    return datetime.strptime(inputs, '%Y-%m-%d %H:%M:%S')
+
+
 def getind():
-    inds = []
+    inds = {}
+    k = 0
     for file in tqdm(os.listdir('./hangye')):
         filename = os.path.join("./hangye", file)
         data = pd.read_csv(filename)
         datacopy = data.copy()
-        datacopy['date'] = pd.to_datetime(data['日期'])
-        df = datacopy.set_index('date')
-        for i in df.index:
+        datacopy['日期'] = data['日期'].apply(convert)
+        limit = 3
+
+        for index, row in datacopy.iterrows():
+            name = row['股票简称']
+            i = str2timedate(row['日期'])
             t1sub365 = sub356(i)
             t1sub90 = sub90(i)
-            company = df["{}".format(t1sub365):"{}".format(t1sub90)]
+            datelist = timestamp2list(pd.date_range(start=t1sub365, end=t1sub90, freq='D'))
+            company = datacopy[datacopy['日期'].isin(datelist)]
+            k += 1
+            del company['股票简称']
+            del company['日期']
+            del company['行业代码']
             means = company.mean().values
-            inds.append(means)
+            inds[name] = means
+            if k > limit:
+                break
     df = pd.DataFrame(inds)
-    df = df.fillna(0)
-    del df[2682]
-    del df[2683]
-    del df[2684]
+    if not os.path.exists('./data_dir'):
+        os.makedirs("./data_dir")
     df.to_csv("./data_dir/ind.csv", index=None, encoding='utf-8')
     print(df.shape)
 
@@ -121,6 +166,7 @@ def getind():
 def buildmodel():
     rec = pd.read_csv("./data_dir/rec.csv")
     ind = pd.read_csv("./data_dir/ind.csv")
+    print(rec.shape, ind.shape)
     data = pd.read_csv("./data_dir/train.csv")
     del data['行业代码']
     del data['日期']
@@ -128,6 +174,10 @@ def buildmodel():
     del data['股票简称']
     length = len(data)
     print("length:{}".format(length))
+    rec = rec.fillna(0)
+    ind = ind.fillna(0)
+    data = data.fillna(0)
+
     savedf = []
     for i in range(length):
         y_train = data.loc[i, :]
@@ -143,25 +193,83 @@ def buildmodel():
 
     df = pd.DataFrame(savedf)
     df['股票简称'] = gupiao
-    df.to_csv("results.csv", encoding='utf-8', index=None,  sep=",")
+    df.to_csv("results.csv", encoding='utf-8', index=None, sep=",")
 
+
+def buildmodel_new():
+    rec = pd.read_csv("./data_dir/rec.csv")
+    ind = pd.read_csv("./data_dir/ind.csv")
+    data = pd.read_csv("./data_dir/train.csv")
+    del data['行业代码']
+    del data['日期']
+    savedf = []
+    print(rec.shape, ind.shape, data.shape)
+
+    rec = rec.fillna(0)
+    ind = ind.fillna(0)
+    data = data.fillna(0)
+    for index, row in data.iterrows():
+        name = row['股票简称']
+        y_train = row.values[1:]
+        x1 = rec[name]
+        x2 = ind[name]
+        print(y_train.shape, x1.shape, x2.shape)
+        X_train = pd.DataFrame([x1, x2]).T.values
+        linreg = LinearRegression()
+        linreg.fit(X_train, y_train)
+        u = linreg.intercept_
+        a1, a2 = linreg.coef_[0], linreg.coef_[1]
+        save = {'a1': a1, 'a2': a2, 'u': u, 'name': name}
+        savedf.append(save)
+
+    df = pd.DataFrame(savedf)
+    df.to_csv("results.csv", encoding='utf-8', index=None, sep=",")
+
+
+def ind_new():
+    data = pd.read_csv("./data_dir/train.csv")
+    inds = {}
+    data['日期'] = data['日期'].apply(convert)
+    for index, row in data.iterrows():
+        i = str2timedate(row['日期'])
+        name = row['股票简称']
+        c = row['行业代码']
+        t1sub365 = sub356(i)
+        t1sub90 = sub90(i)
+        datelist = timestamp2list(pd.date_range(start=t1sub365, end=t1sub90, freq='D'))
+        company = data[data['日期'].isin(datelist) & data['行业代码'].isin([c])]
+        means = company.mean()
+        inds[name] = means
+        print(np.sum(means))
+    df = pd.DataFrame(inds)
+    if not os.path.exists('./data_dir'):
+        os.makedirs("./data_dir")
+    df.to_csv("./data_dir/ind.csv", index=None, encoding='utf-8')
+    print(df.shape)
 
 if __name__ == '__main__':
     # 这里依次修改 下面的每种method，就可以出结果了。
 
-    method = 'buildmodel'  # groupbycompany，rec_compute， groupbyhangye， getind， buildmodel
-    if method == 'groupbycompany':
-        groupbycompany()
-
-    if method == 'rec_compute':
-        rec_compute()
+    method = 'ind_new'  # 0.groupbyhangye  1. getind, 2. rec_new, 3. buildmodel_new
+    # if method == 'groupbycompany':
+    #     groupbycompany()
+    #
+    # if method == 'rec_compute':
+    #     rec_compute()
 
     if method == 'groupbyhangye':
         groupbyhangye()
 
     if method == 'getind':
-        getind()
+        getind()  # (478, 2682)
 
-    if method == 'buildmodel':
-        buildmodel()
+    # if method == 'buildmodel':
+    #     buildmodel()
 
+    if method == 'rec_new':
+        rec_new()
+
+    if method == 'buildmodel_new':
+        buildmodel_new()
+    if method == 'ind_new':
+        ind_new()
