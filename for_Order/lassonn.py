b572a4d30c3025b6e14dd79c4962e014580943e2
duyanpu <duyanpu_dz@mayi.com>
2018-04-09 18:48:26 +0800

    ss

--- for_Order/lassonn.py
+++ for_Order/lassonn.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import math
@@ -7,6 +7,7 @@ import os
 from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LassoCV, Lars, lars_path
 from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
 
 da = pd.read_csv("./datasets/libai.csv")
 da.columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10',
@@ -36,7 +37,6 @@ def lasso_features_selection():
 
 def nn():
     # 神经网络模型
-    from sklearn.model_selection import train_test_split
     data = pd.read_csv("./datasets/lasso_select.csv")
     y = data['y']
     del data['y']
@@ -74,9 +74,8 @@ def nn():
     # 这里在datasets目录下，保存了一份预测的结果文件。
 
 
-def huiseyuce(history_data):
+def huiseyuce(history_data, m=4):
     # 灰色预测模型
-    history_data, h_test = history_data[0:12], history_data[12:]
     X0 = np.array(history_data)
     n = len(history_data)
 
@@ -141,38 +140,69 @@ def huiseyuce(history_data):
 
     if C < 0.35 and P > 0.95:
         # 预测精度为一级
-        m = 4  # 请输入需要预测的年数  [ 394.00901644  398.77168963  403.59193271  408.47044156  413.4079205 ]
         f = np.zeros(m)
         for i in range(0, m):
             f[i] = (X0[0] - u / a) * (1 - math.exp(a)) * math.exp(-a * (i + n))
-
-        print(f)
-        print(h_test.values)
-        df = pd.DataFrame()
-        df['y'] = h_test.values
-        df['y_pred'] = f
-        df.to_csv("./datasets/huiseyuce_results.csv", index=None)
-
+        return f
     else:
         print('灰色预测法不适用')
 
 
 def build_huiseyuce():
+    data = pd.read_csv("./datasets/lasso_select.csv")
+    predicts = []
+    for ix, col in data.iteritems():
+        res = huiseyuce(col.values)
+        predicts.append(res)
+    df = pd.DataFrame(predicts).T
+    df.columns = data.columns
+    newdata = pd.concat([data, df])
+    return newdata
 
-    data = pd.read_csv("./datasets/lasso_select.csv", usecols=['x6', 'x11'])
-    x6 = data['x6']
-    x11 = data['x11']
-    huiseyuce(x6)
-    huiseyuce(x11)
+
+def build_nn_huiseyuce():
+    data = build_huiseyuce()
+    y = data['y']
+    del data['y']
+    X = data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+    # 下面的参数都可以调，尝试修改不同的值，看看预测的结果会不会变好。
+    mlp = MLPRegressor(hidden_layer_sizes=(200000,), activation="tanh",
+                       solver='adam', alpha=0.01,
+                       batch_size='auto', learning_rate="constant",
+                       learning_rate_init=0.01,
+                       power_t=0.5, max_iter=1000, shuffle=True,
+                       random_state=None, tol=1e-4,
+                       verbose=False, warm_start=False, momentum=0.9,
+                       nesterovs_momentum=True, early_stopping=False,
+                       validation_fraction=0.1, beta_1=0.9, beta_2=0.999,
+                       epsilon=1e-8)
+    mlp.fit(X_train, y_train)
+    loss = mlp.loss_
+    print(loss, len(str(loss).split(".")[0]))
+    # 这里主要看loss的值，越小越好
+    y_pred = mlp.predict(X_test)
+
+    predicts = pd.DataFrame()
+    predicts['true'] = y_test
+    predicts['pred'] = y_pred
+    predicts.to_csv("./datasets/predicts_total_values.csv", index=None)
+    # 这里在datasets目录下，保存了一份预测的结果文件。
 
 
 if __name__ == "__main__":
-    method = 'lasso_features_selection'
+    method = 'build_nn_huiseyuce'
     # 选择lasso_features_selection, nn, huiseyuce中的一个，执行下面的代码
 
     if method == 'lasso_features_selection':
         lasso_features_selection()
     if method == 'nn':
         nn()
-    if method == 'huiseyuce':
-        huiseyuce()
+
+    if method == 'build_huiseyuce':
+        build_huiseyuce()
+
+    if method == 'build_nn_huiseyuce':
+        build_nn_huiseyuce()
