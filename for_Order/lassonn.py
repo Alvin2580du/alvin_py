59df84f653c4a5fd036ab37b87515a747f970c98
Alvin2580du <ypducdtu@163.com>
2018-04-09 20:56:08 +0800

    update

--- for_Order/lassonn.py
+++ for_Order/lassonn.py
@@ -1,11 +1,9 @@
-# import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import math
 import os
 
-from sklearn.feature_selection import SelectFromModel
-from sklearn.linear_model import LassoCV, Lars, lars_path
+from sklearn.linear_model import LassoCV
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 
@@ -15,41 +13,40 @@ da.columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10',
 y = da['y']
 del da['y']
 X = da
-print(X.shape, y.shape)
 
 
 def lasso_features_selection():
-    lassocv = LassoCV()
+    lassocv = LassoCV(eps=0.0002, n_alphas=100, alphas=None, fit_intercept=True,
+                      normalize=False, precompute='auto', max_iter=1000, tol=1e-4,
+                      copy_X=True, cv=None, verbose=False, n_jobs=1,
+                      positive=False, random_state=None, selection='cyclic')
     lassocv.fit(X, y)
     mask = lassocv.coef_ != 0
-    print("mask:{}".format(mask), type(mask), len(mask))
     new_reg_data = X.loc[:, mask]
-    print(new_reg_data.shape)
-    print(type(new_reg_data), new_reg_data)
     ydf = pd.DataFrame(y)
     df = pd.concat([new_reg_data, ydf], axis=1)
     if not os.path.exists("./datasets"):
         os.makedirs("./datasets")
 
-    df.to_csv("./datasets/lasso_select.csv", index=None)
     # 选择后的文件 保存到datasets 目录下面
+    df.to_csv("./datasets/lasso_select.csv", index=None)
 
 
-def nn():
+def onlynn():
     # 神经网络模型
     data = pd.read_csv("./datasets/lasso_select.csv")
     y = data['y']
     del data['y']
     X = data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
     print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
     # 下面的参数都可以调，尝试修改不同的值，看看预测的结果会不会变好。
-    mlp = MLPRegressor(hidden_layer_sizes=(200000,), activation="tanh",
+    mlp = MLPRegressor(hidden_layer_sizes=(300000,), activation="tanh",
                        solver='adam', alpha=0.01,
                        batch_size='auto', learning_rate="constant",
                        learning_rate_init=0.01,
-                       power_t=0.5, max_iter=1000, shuffle=True,
+                       power_t=0.5, max_iter=10, shuffle=True,
                        random_state=None, tol=1e-4,
                        verbose=False, warm_start=False, momentum=0.9,
                        nesterovs_momentum=True, early_stopping=False,
@@ -59,14 +56,7 @@ def nn():
     loss = mlp.loss_
     print(loss, len(str(loss).split(".")[0]))
     # 这里主要看loss的值，越小越好
-    # coefs = mlp.coefs_
-    # inter = mlp.intercepts_
-    # n_iter = mlp.n_iter_
-    # nlayers = mlp.n_layers_
-    # noutputs = mlp.n_outputs_
-    # print(loss, coefs, n_iter, inter, nlayers, noutputs)
     y_pred = mlp.predict(X_test)
-
     predicts = pd.DataFrame()
     predicts['true'] = y_test
     predicts['pred'] = y_pred
@@ -148,27 +138,40 @@ def huiseyuce(history_data, m=4):
         print('灰色预测法不适用')
 
 
+def only_gm11():
+    da = pd.read_csv("./datasets/libai.csv")
+    da.columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10',
+                  'x11', 'x12', 'x13', 'y']
+    y = da['y']
+    train_y, test_y = y[:12], y[12:].values
+    results = huiseyuce(train_y)
+    print(results)
+    print(test_y)
+    df = np.array([results, test_y])
+    df = pd.DataFrame(df).T
+    df.columns = ['pred', 'true']
+    df.to_csv('./datasets/only_gm11_results.csv', index=None)
+
+
 def build_huiseyuce():
     data = pd.read_csv("./datasets/lasso_select.csv")
     predicts = []
     for ix, col in data.iteritems():
-        res = huiseyuce(col.values)
+        res = huiseyuce(col.values, m=1)
         predicts.append(res)
     df = pd.DataFrame(predicts).T
     df.columns = data.columns
-    newdata = pd.concat([data, df])
-    return newdata
+    del df['y']
+    return df.values
 
 
 def build_nn_huiseyuce():
-    data = build_huiseyuce()
+    data = pd.read_csv("./datasets/lasso_select.csv")
     y = data['y']
     del data['y']
     X = data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
     print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-
-    # 下面的参数都可以调，尝试修改不同的值，看看预测的结果会不会变好。
     mlp = MLPRegressor(hidden_layer_sizes=(200000,), activation="tanh",
                        solver='adam', alpha=0.01,
                        batch_size='auto', learning_rate="constant",
@@ -182,27 +185,32 @@ def build_nn_huiseyuce():
     mlp.fit(X_train, y_train)
     loss = mlp.loss_
     print(loss, len(str(loss).split(".")[0]))
-    # 这里主要看loss的值，越小越好
-    y_pred = mlp.predict(X_test)
 
+    # 后面四年作检验
+    y_pred = mlp.predict(X_test)
     predicts = pd.DataFrame()
     predicts['true'] = y_test
     predicts['pred'] = y_pred
-    predicts.to_csv("./datasets/predicts_total_values.csv", index=None)
     # 这里在datasets目录下，保存了一份预测的结果文件。
+    predicts.to_csv("./datasets/predicts_nn_and_gm11.csv", index=None)
+
+    # 预测17年的
+    lastdata = build_huiseyuce()
+    y_pred_17 = mlp.predict(lastdata)[0]
+    print(y_pred_17)
 
 
 if __name__ == "__main__":
-    method = 'build_nn_huiseyuce'
-    # 选择lasso_features_selection, nn, huiseyuce中的一个，执行下面的代码
+    method = 'only_gm11'
+    # 选择lasso_features_selection, nn, build_nn_huiseyuce中的一个，执行下面的代码
 
     if method == 'lasso_features_selection':
         lasso_features_selection()
-    if method == 'nn':
-        nn()
 
-    if method == 'build_huiseyuce':
-        build_huiseyuce()
+    if method == 'onlynn':
+        onlynn()
 
+    if method == 'only_gm11':
+        only_gm11()
     if method == 'build_nn_huiseyuce':
         build_nn_huiseyuce()
