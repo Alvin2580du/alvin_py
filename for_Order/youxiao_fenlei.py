96e4f26ceffca77d77504cc1095e899c566ec306
duyanpu <duyanpu_dz@mayi.com>
2018-06-23 18:25:21 +0800

    机器学习 整合

--- for_Order/youxiao_fenlei.py
+++ for_Order/youxiao_fenlei.py
@@ -32,7 +32,7 @@ except:
 test_rate = 0.3
 x_train, x_test, y_train, y_test = train_test_split(outfile, y_onehot, test_size=test_rate)
 
-# KNeighborsClassifier
+# KNeighborsClassifier，K近邻算法
 nflod_1 = 5
 cv_1 = ShuffleSplit(n_splits=nflod_1, test_size=test_rate, random_state=1)
 rf_param_1 = {'n_neighbors': [1, 2, 3, 4, 5]}
@@ -47,8 +47,8 @@ result_1 = pd.DataFrame()
 result_1['p'] = predictions_1
 result_1['t'] = y_test
 result_1.to_csv("result_1.csv", index=None)
-
-# LogisticRegression
+print("结果1保存成功")
+# LogisticRegression，逻辑回归算法
 nflod_2 = 5
 cv_2 = ShuffleSplit(n_splits=nflod_2, test_size=test_rate, random_state=1)
 rf_param_2 = {'penalty': ['l1', 'l2']}
@@ -65,8 +65,9 @@ result_2 = pd.DataFrame()
 result_2['p'] = predictions_2
 result_2['t'] = y_test
 result_2.to_csv("result_2.csv", index=None)
+print("结果2保存成功")
 
-# DecisionTreeClassifier
+# DecisionTreeClassifier， 决策树算法
 nflod_3 = 5
 cv_3 = ShuffleSplit(n_splits=nflod_2, test_size=test_rate, random_state=1)
 rf_param_3 = {'criterion': ['entropy', 'gini']}
@@ -92,10 +93,11 @@ result_3 = pd.DataFrame()
 result_3['p'] = predictions_3
 result_3['t'] = y_test
 result_3.to_csv("result_3.csv", index=None)
+print("结果3保存成功")
 
 # 特征选择和集成学习
 my_classifier_4 = AdaBoostClassifier()
-rfe = RFE(my_classifier_4, n_features_to_select=2)
+rfe = RFE(my_classifier_4, n_features_to_select=3)
 rfe.fit(x, y)
 predictions_4 = rfe.predict(x_test)
 
@@ -103,6 +105,7 @@ result_4 = pd.DataFrame()
 result_4['p'] = predictions_4
 result_4['t'] = y_test
 result_4.to_csv("result_4.csv", index=None)
+print("结果4保存成功")
 
 names = iris["feature_names"]
 print("选择的特征是：\n")
