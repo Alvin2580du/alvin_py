5e27f11ea274c6825ce57399c216c43ff4df7f55
Alvin2580du <ypducdtu@163.com>
2018-08-19 22:48:28 +0800

    useless

--- resources/tmp.py
+++ resources/tmp.py
@@ -1,4 +1,29 @@
-
 import nltk
+from nltk.corpus import gutenberg
+print(dir(gutenberg))
+exit(1)
+import en_core_web_sm
 
-nltk.download()
+nlp = en_core_web_sm.load()
+raw = gutenberg.raw("burgess-busterbrown.txt")
+
+def fun1():
+    reader = gcr.GutenbergCorpusReader()
+    print(reader)
+    authors = reader.get_authors()
+
+    print('The number of authors mentioned in the Gutenberg corpus are:', format(len(authors)))
+    for author in authors:
+        print("{0}: {1}".format(author, len(authors[author])))
+
+    authors = reader.get_authors()
+    tot = 0
+    for author in authors:
+        tot += len(authors[author])
+    works = reader.get_authors_works('De Mille, James')
+    for work in works:
+        print(work["title"])
+    parsed_novel = nlp(works[7]["text"])
+    return parsed_novel
+
+fun1()
